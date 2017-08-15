3d396b777549e8d669e6fa0ec49369f12f011ffd
lightcode <lightcode@gmx.com>
2017-08-15 22:37:14 +0200

    Clean and comments

--- cpu.v
+++ cpu.v
@@ -1,41 +1,54 @@
 module cpu;
 
+  wire clock;
   wire [7:0] bus;
 
   reg reset = 0;
 
-  // Define clock
+  // ==========================
+  // System clock
+  // ==========================
+
   clock sys_clk (clk);
 
-  // Define A register
+
+  // ==========================
+  // Registers
+  // ==========================
+
+  // A register
   wire [7:0] regA_bus;
   reg c_ao = 0;
   reg c_ai = 0;
   register regA (bus, clk, c_ai, reset, regA_bus);
   tristate_buffer regA_buff (regA_bus, c_ao, bus);
 
-  // Define B register
+  // B register
   wire [7:0] regB_bus;
   reg c_bo = 0;
   reg c_bi = 0;
   register regB (bus, clk, c_bi, reset, regB_bus);
   tristate_buffer regB_buff (regB_bus, c_bo, bus);
 
-  // Define Z register
+  // Z register
   wire [7:0] regZ_bus;
   reg c_zo = 0;
   reg c_zi = 0;
   register regZ (bus, clk, c_zi, reset, regZ_bus);
   tristate_buffer regZ_buff (regZ_bus, c_zo, bus);
 
-  // Define instruction register
+  // Instruction Register
   wire [7:0] regI_bus;
   reg c_io = 0;
   reg c_ii = 0;
   register regI (bus, clk, c_ii, reset, regI_bus);
   tristate_buffer regI_buff (regI_bus, c_io, bus);
 
-  // Define PC
+
+  // ==========================
+  // Program Counter
+  // ==========================
+
   wire [7:0] pc_out;
   reg c_co = 0;
   reg c_ce = 0;
@@ -43,12 +56,21 @@ module cpu;
   counter pc (c_ce, bus, c_j, reset, pc_out);
   tristate_buffer pc_buff (pc_out, c_co, bus);
 
+
+  // ==========================
   // ALU
+  // ==========================
+
   reg cin = 0;
   reg c_eo = 0;
   wire [7:0] sum;
   alu alu (cin, cout, regA_bus, regB_bus, sum);
   tristate_buffer sum_buff (sum, c_eo, bus);
+
+
+  // ==========================
+  // Tests
+  // ==========================
 
   initial begin
     $monitor("At time %t, bus = %h, sum = %d, regA = %d, regB = %d, pc_out = %d", $time, bus, sum, regA_bus, regB_bus, pc_out);
