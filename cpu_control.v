a5e3263300e391bc377a24256a6eb6bdc0b96069
lightcode <lightcode@gmx.com>
2017-08-21 22:56:20 +0200

    Rewrite CPU FSM without if/else

--- cpu_control.v
+++ cpu_control.v
@@ -11,28 +11,20 @@ module cpu_control(
     case (cycle)
       0: state = `STATE_FETCH_PC;
       1: state = `STATE_FETCH_INST;
-      2: begin
-        if (opcode == `OP_HLT)
-          state = `STATE_HALT;
-        else if (opcode == `OP_OUT)
-          state = `STATE_OUT_A;
-        else
-          state = `STATE_FETCH_PC;
-      end
-      3: begin
-        if (opcode == `OP_HLT || opcode == `OP_OUT)
-          state = `STATE_NEXT;
-        else if (opcode == `OP_JEZ && !eq_zero || opcode == `OP_JNZ && eq_zero)
-          state = `STATE_SKIP_JUMP;
-        else if (opcode == `OP_JMP || opcode == `OP_JEZ && eq_zero || opcode == `OP_JNZ && !eq_zero)
-          state = `STATE_JUMP;
-        else
-          state = `STATE_LOAD_ADDR;
-      end
-      4: state = (opcode == `OP_JEZ || opcode == `OP_JNZ) ? `STATE_NEXT :
-                 (opcode == `OP_LDA) ? `STATE_RAM_A :
-                 (opcode == `OP_STA) ? `STATE_STORE_A : `STATE_RAM_B;
-      5: state = (opcode == `OP_LDA) ? `STATE_NEXT : (opcode == `OP_ADD) ? `STATE_ADD : `STATE_SUB;
+      2: state = (opcode == `OP_HLT) ? `STATE_HALT :
+                 (opcode == `OP_OUT) ? `STATE_OUT_A :
+                 `STATE_FETCH_PC;
+      3: state = (opcode == `OP_HLT || opcode == `OP_OUT) ? `STATE_NEXT :
+                 (opcode == `OP_JEZ && !eq_zero || opcode == `OP_JNZ && eq_zero) ? `STATE_SKIP_JUMP :
+                 (opcode == `OP_JMP || opcode == `OP_JEZ && eq_zero || opcode == `OP_JNZ && !eq_zero) ? `STATE_JUMP :
+                 `STATE_LOAD_ADDR;
+      4: state = (opcode == `OP_LDA) ? `STATE_RAM_A :
+                 (opcode == `OP_STA) ? `STATE_STORE_A :
+                 (opcode == `OP_ADD || opcode == `OP_SUB) ?`STATE_RAM_B :
+                 `STATE_NEXT;
+      5: state = (opcode == `OP_LDA) ? `STATE_NEXT :
+                 (opcode == `OP_ADD) ? `STATE_ADD :
+                 `STATE_SUB;
       6: state = `STATE_NEXT;
       default: $display("Cannot decode : cycle = %d, opcode = %h", cycle, opcode);
     endcase
